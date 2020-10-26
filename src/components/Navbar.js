4e850520b40c2bf839158034d949f62113069cd6
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-25 23:19:51 -0300

    feat: changed title and added favicon
    Signed-off-by: Joao Pedro Prado Leal <joaopedro0128@hotmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,18 @@ export default function Navbar() {
     <nav id="navbar">
       <div className="nav-links">
         <Link to="/" className="link">Conheça</Link>  
-        <Link to="/" className="link">Calculadoras</Link>  
-        <Link to="/" className="link">Materiais</Link>
+        <Link 
+          to="https://aps.bvs.br/apps/calculadoras/?page=7" 
+          className="link"
+          rel="noreferrer noopener"
+          target="_blank"
+        >Calculadoras</Link>  
+        <Link 
+          to="/https://pebmed.com.br/veja-o-calendario-vacinal-do-ministerio-da-saude-para-2020/" 
+          className="link"
+          rel="noreferrer noopener"
+          target="_blank"
+        >Materiais</Link>
       </div>
       <div className="user-controls">
         <Link to="/login" className="link">Entrar</Link>
